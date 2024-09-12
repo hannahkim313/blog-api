ae9bd6fc4fe827bf5e9daacc360d697648178e44
Hannah <67137839+hannahkim313@users.noreply.github.com>
2024-09-12 16:56:48 -0700

    Implement the GET request to retrieve a paginated list of all articles

--- src/controllers/articlesController.js
+++ src/controllers/articlesController.js
@@ -1,6 +1,24 @@
-const articlesGetAll = (req, res) => {
-  res.send('not yet implemented');
-};
+const asyncHandler = require('express-async-handler');
+const prisma = require('../../prisma/prismaClient');
+const sendResponse = require('../utils/sendResponse');
+
+const articlesGetAll = asyncHandler(async (req, res) => {
+  const { page = 1, pageSize = 10 } = req.query;
+
+  const articles = await prisma.article.findMany({
+    skip: (page - 1) * pageSize,
+    take: parseInt(pageSize),
+    select: { id: true, title: true },
+  });
+
+  const totalArticles = await prisma.article.count();
+
+  sendResponse(res, 200, {
+    articles,
+    totalPages: Math.ceil(totalArticles / pageSize),
+    currentPage: parseInt(page),
+  });
+});
 
 const articlesCreate = (req, res) => {
   res.send('not yet implemented');
--- src/routes/articlesRouter.js
+++ src/routes/articlesRouter.js
@@ -4,9 +4,13 @@ const articlesController = require('../controllers/articlesController');
 const articlesRouter = Router();
 
 articlesRouter.get('/', articlesController.articlesGetAll);
+
 articlesRouter.post('/', articlesController.articlesCreate);
+
 articlesRouter.get('/:articleId', articlesController.articlesGetById);
+
 articlesRouter.put('/:articleId', articlesController.articlesUpdateById);
+
 articlesRouter.delete('/:articleId', articlesController.articlesDeleteById);
 
 module.exports = articlesRouter;
