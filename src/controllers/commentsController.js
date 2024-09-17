af983d345df95f206cee36cc471a4dcecfa4c8a2
Hannah <67137839+hannahkim313@users.noreply.github.com>
2024-09-16 22:03:06 -0700

    Retrieve all comments of a specific article
    This commit allows anyone to access all comments of published articles and users with the role 'author' to also have access to all comments of unpublished articles.

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -1,6 +1,50 @@
-const commentsGetAll = (req, res) => {
-  res.send('not yet implemented');
-};
+const asyncHandler = require('express-async-handler');
+const prisma = require('../../prisma/prismaClient');
+const sendResponse = require('../utils/sendResponse');
+
+const commentsGetAll = asyncHandler(async (req, res) => {
+  const articleId = parseInt(req.params.articleId, 10);
+  const { role } = req.user;
+  const isAuthor = role === 'author';
+
+  const article = await prisma.article.findUnique({
+    where: {
+      id: articleId,
+      isPublished: !isAuthor ? true : {},
+    },
+  });
+
+  if (!article) {
+    return sendResponse(res, 404);
+  }
+
+  const { page = 1, pageSize = 10 } = req.query;
+
+  const comments = await prisma.comment.findMany({
+    skip: (page - 1) * pageSize,
+    take: parseInt(pageSize, 10),
+    select: {
+      id: true,
+      content: true,
+      user: {
+        select: {
+          firstName: true,
+        },
+      },
+    },
+    where: { articleId: article.id },
+  });
+
+  const totalComments = await prisma.comment.count({
+    where: { articleId: article.id },
+  });
+
+  sendResponse(res, 200, {
+    comments,
+    totalPages: Math.ceil(totalComments / pageSize),
+    currentPage: parseInt(page, 10),
+  });
+});
 
 const commentsCreate = (req, res) => {
   res.send('not yet implemented');
--- src/routes/commentsRouter.js
+++ src/routes/commentsRouter.js
@@ -1,9 +1,16 @@
 const { Router } = require('express');
 const commentsController = require('../controllers/commentsController');
+const { validateArticleId } = require('../validators/articleValidators');
+const verifyToken = require('../middleware/verifyToken');
 
-const commentsRouter = Router();
+const commentsRouter = Router({ mergeParams: true });
 
-commentsRouter.get('/', commentsController.commentsGetAll);
+commentsRouter.get(
+  '/',
+  verifyToken,
+  validateArticleId,
+  commentsController.commentsGetAll
+);
 commentsRouter.post('/', commentsController.commentsCreate);
 commentsRouter.put('/:commentId', commentsController.commentsUpdateById);
 commentsRouter.delete('/:commentId', commentsController.commentsDeleteById);
